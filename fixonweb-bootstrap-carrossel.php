eb8e6e367cfd5910f109ea7c6224898dd3518b7b
fixonweb <contato@fixonweb.com.br>
2020-05-14 14:30:26 -0300

    commit 2

--- fixonweb-bootstrap-carrossel.php
+++ fixonweb-bootstrap-carrossel.php
@@ -7,7 +7,7 @@
  * Author URI:      https://github.com/fixonweb
  * Text Domain:     fixonweb-bootstrap-carrossel
  * Domain Path:     /languages
- * Version:         0.1.0
+ * Version:         0.1.1
  *
  * @package         Fixonweb_Bootstrap_Carrossel
  */
@@ -16,3 +16,8 @@
 
 require 'plugin-update-checker.php';
 $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker('https://github.com/FIXONWEB/fixonweb-bootstrap-carrossel', __FILE__, 'fixonweb-bootstrap-carrossel/fixonweb-bootstrap-carrossel');
+
+require_once('src/cptbc-init.php');
+require_once('src/cptbc-admin.php');
+require_once('src/cptbc-settings.php');
+require_once('src/cptbc-frontend.php');
